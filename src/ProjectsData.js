811e3d25be3e615ff378105b713d6373ebbfb980
majdrammal <majdrammal2001@gmail.com>
2022-08-01 15:58:23 +0300

    Updated projects section

--- src/ProjectsData.js
+++ src/ProjectsData.js
@@ -1,18 +1,18 @@
-import blinker from './assets/blinker mockup.png'
+import maintenance from './assets/maintenance.jpg'
 
 
 export const data = [
-    {img: "https://v-lawoffice.ro/construction.jpg", 
+    {img: maintenance, 
     title: "Haroun Law Firm", 
     tech: "HTML, CSS, Javascript and React", 
     desc: "Currently designing and developing the Haroun Law Firm website."
     },
-    {img: "https://v-lawoffice.ro/construction.jpg", 
+    {img: maintenance, 
     title: "AUB Phoenix Project", 
     tech: "HTML, CSS, Javascript and React", 
     desc: "Profits from the AUB Phoenix Project will be used to fund scolarship programs at the American University of Beirut. I am currently part of the team of AUB students responsible for creating the project's website."
     },
-    {img: "https://v-lawoffice.ro/construction.jpg", 
+    {img: maintenance, 
     title: "House of Flicks", 
     tech: "HTML, CSS, Javascript, React and Firebase", 
     desc: "Similar to Letterboxd, House of Flicks is a social media platform for Film & TV show enthusiasts. This is a personal project."
