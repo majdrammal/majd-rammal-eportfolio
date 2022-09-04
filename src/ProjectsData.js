cbfafce1a8c13b715790f921a4bcd9c6553ed518
majdrammal <majdrammal2001@gmail.com>
2022-09-04 23:43:23 +0300

    Changed haroun law firm tech

--- src/ProjectsData.js
+++ src/ProjectsData.js
@@ -5,24 +5,24 @@ import haroun from'./assets/haroun.jpg'
 export const data = [
     {img: haroun, 
     title: "Haroun Law Firm", 
-    tech: "HTML, CSS, Javascript and React", 
+    tech: "HTML, CSS, TypeScript and React", 
     desc: "Solely designing and developing the Haroun Law Firm website.",
     link: "https://haroun-law-firm.herokuapp.com/"
     },
     {img: maintenance, 
     title: "AUB Phoenix Project", 
-    tech: "HTML, CSS, Javascript, React and Firebase", 
+    tech: "HTML, CSS, JavaScript, React and Firebase", 
     desc: "Profits from the AUB Phoenix Project will be used to fund scolarship programs at the American University of Beirut. I am currently working with a team of AUB students as the head Full-Stack developer responsible for creating the project's website."
     },
     {img: flix, 
     title: "Flix", 
-    tech: "HTML, CSS, Sass, Javascript, React and Firebase", 
+    tech: "HTML, CSS, Sass, JavaScript, React and Firebase", 
     desc: "A social media platform that allows users to search for their favorite movies, keep track of what they've watched, and connect with friends. This is a personal and non-commercial project.",
     link: "https://the-flix.herokuapp.com/"
     },
     {img: maintenance, 
     title: "Reach", 
-    tech: "HTML, CSS, Sass, Javascript, React, React Native and Firebase", 
+    tech: "HTML, CSS, Sass, JavaScript, React, React Native and Firebase", 
     desc: "A multi-platform application that enables users to instant message and connect with each other through their computers or mobile devices.",
     }
 ]
