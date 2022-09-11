5a58137351822023601a1c1bb3288fc0d4165d07
majdrammal <majdrammal2001@gmail.com>
2022-09-11 21:39:14 +0300

    Added aub small businesses project

--- src/ProjectsData.js
+++ src/ProjectsData.js
@@ -4,32 +4,44 @@ import haroun from './assets/haroun.jpg'
 import portfolio from './assets/portfolio.jpg'
 
 export const data = [
-    {img: haroun, 
-    title: "Haroun Law Firm", 
-    tech: "HTML, CSS, TypeScript and React", 
-    desc: "Solely designing and developing the Haroun Law Firm website.",
-    link: "https://haroun-law-firm.herokuapp.com/"
+    {
+        img: portfolio, 
+        title: "Personal Website", 
+        tech: "HTML, CSS, JavaScript and React",
+        link: "https://www.majdrammal.com"
     },
-    {img: maintenance, 
-    title: "AUB Phoenix Project", 
-    tech: "HTML, CSS, JavaScript, React and Firebase", 
-    desc: "Profits from the AUB Phoenix Project will be used to fund scolarship programs at the American University of Beirut. I am currently working with a team of AUB students as the head Full-Stack developer responsible for creating the project's website."
+    {
+        img: haroun, 
+        title: "Haroun Law Firm", 
+        tech: "HTML, CSS, TypeScript and React", 
+        desc: "Solely designing and developing the Haroun Law Firm website.",
+        link: "https://haroun-law-firm.herokuapp.com/"
     },
-    {img: flix, 
-    title: "Flix", 
-    tech: "HTML, CSS, Sass, JavaScript, React and Firebase", 
-    desc: "A social media platform that allows users to search for their favorite movies, keep track of what they've watched, and connect with friends. This is a personal and non-commercial project.",
-    link: "https://the-flix.herokuapp.com/"
+    {
+        img: flix, 
+        title: "Flix", 
+        tech: "HTML, CSS, Sass, JavaScript, React and Firebase", 
+        desc: "A social media platform that allows users to search for their favorite movies, keep track of what they've watched, and connect with friends. This is a personal and non-commercial project.",
+        link: "https://the-flix.herokuapp.com/"
     },
-    {img: maintenance, 
-    title: "Reach", 
-    tech: "HTML, TypeScript, React, React Native and Firebase", 
-    desc: "A multi-platform application that enables users to instant message and connect with each other through their computers or mobile devices.",
+    {
+        img: maintenance,
+        title: 'AUB Small Businesses',
+        tech: 'HTML, CSS, JavaScript and React',
+        desc: "Solely designing and developing an AUB-backed website that showcases AUB students' small businesses.",
+        link:"https://github.com/majdrammal/small-businesses"
     },
-    {img: portfolio, 
-    title: "Personal Website", 
-    tech: "HTML, CSS, JavaScript and React",
-    link: "https://www.majdrammal.com"
+    {
+        img: maintenance, 
+        title: "AUB Phoenix Project", 
+        tech: "HTML, CSS, JavaScript, React and Firebase", 
+        desc: "Profits from the AUB Phoenix Project will be used to fund scolarship programs at the American University of Beirut. I am currently working with a team of AUB students as the head Full-Stack developer responsible for creating the project's website."
+    },
+    {
+        img: maintenance, 
+        title: "Reach", 
+        tech: "HTML, TypeScript, React, React Native and Firebase", 
+        desc: "A multi-platform application that enables users to instant message and connect with each other through their computers or mobile devices.",
     }
 ]
 
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NextPage from './ui/NextPage';
 import PrevPage from './ui/PrevPage';
 import Project from './ui/Project';
@@ -28,6 +28,7 @@ const Projects = () => {
                     <Project data={data} num={2} />
                     <Project data={data} num={3} />
                     <Project data={data} num={4} />
+                    <Project data={data} num={5} />
                 </ul>
                 <div className="project__hr--list">
                     <hr className="project__hr project__hr--0" onClick = {() => projectOpen(0)} />
@@ -35,6 +36,7 @@ const Projects = () => {
                     <hr className="project__hr project__hr--2" onClick = {() => projectOpen(2)}/>
                     <hr className="project__hr project__hr--3" onClick = {() => projectOpen(3)}/>
                     <hr className="project__hr project__hr--4" onClick = {() => projectOpen(4)}/>
+                    <hr className="project__hr project__hr--5" onClick = {() => projectOpen(5)}/>
                 </div>
             </div>
             <PrevPage text="stack" link="stack" />
